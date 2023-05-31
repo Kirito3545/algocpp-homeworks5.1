f223a0f76ab44c9ab610ddf51f43c7d5e1b032b0
Mikhail Dubrovskiy <dmaz68@mail.ru>
2023-05-31 23:56:07 +0300

    Initial commit

---
+++ algocpp-homeworks5.1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include "print.h"
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::srand(std::time(nullptr));
+
+    int arrSize = 3 + rand() % 17;
+
+    
+    int* arr = new int[arrSize] {};
+
+   
+    for (int i = 0; i < arrSize; i++)
+    {
+        arr[i] = rand() % 55;
+    }
+
+    PrintArray(arr, arrSize);
+    PrintPyramid(arr, arrSize);
+
+    delete[] arr;
+    arr = nullptr;
+}
